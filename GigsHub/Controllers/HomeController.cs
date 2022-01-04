9aed634ccd4feaec8695427f85c00e1528101128
Islam Hamed <islamhamed289@gmail.com>
2022-01-04 18:19:55 +0200

    Initial commit

--- GigsHub/Controllers/HomeController.cs
+++ GigsHub/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace GigsHub.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your page.";
 
             return View();
         }
