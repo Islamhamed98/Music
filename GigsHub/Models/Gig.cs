dc2b4da1a0a4bf35046061e334eac3e8cb6888c4
Islam Hamed <islamhamed289@gmail.com>
2022-01-05 02:36:10 +0200

    Populate Genres in Database

--- GigsHub/Models/Gig.cs
+++ GigsHub/Models/Gig.cs
@@ -6,12 +6,16 @@ namespace GigsHub.Models
     public class Gig
     {
         public int Id { get; set; }
+        
         [Required]
         public ApplicationUser Artist { get; set; }
+        
         public DateTime DateTime { get; set; } 
+        
         [Required]
         [StringLength(255)]
         public string Venue { get; set; } 
+
         [Required]
         [StringLength(255)]
         public Genre Genre { get; set; } 
