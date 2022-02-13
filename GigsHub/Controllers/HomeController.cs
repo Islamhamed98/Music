b455b1ce54164eeb63301eedddea1c46c9502e38
Islam Hamed <islamhamed289@gmail.com>
2022-02-13 14:12:43 +0200

    Some Beautiful tips for style of application.

--- GigsHub/Controllers/HomeController.cs
+++ GigsHub/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace GigsHub.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g=>g.Genre)
                 .Where(g=>g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
--- GigsHub/Models/IdentityModels.cs
+++ GigsHub/Models/IdentityModels.cs
@@ -26,15 +26,24 @@ namespace GigsHub.Models
     {   
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre>Genres { get; set; }
-
+        public DbSet<Attendance> Attendances { get; set; }
+        
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+                
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
